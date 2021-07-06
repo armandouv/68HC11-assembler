10193df85d07cd5cf48c00ef981cf4e6190573cb
Armando Ugalde <armauv@gmail.com>
2021-07-06 15:44:54 -0500

    Add compile skeleton

--- src/main/java/eypdc/assembler/Assembler.java
+++ src/main/java/eypdc/assembler/Assembler.java
@@ -1,4 +1,8 @@
 package eypdc.assembler;
 
 public class Assembler {
+
+    public static void compile(InstructionSet instructionSet, String sourcePath) {
+
+    }
 }
--- src/main/java/eypdc/assembler/Main.java
+++ src/main/java/eypdc/assembler/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        if (args.length != 2) throw new IllegalArgumentException("Usage: program <filepath>");
         InstructionSet instructionSet = InstructionSet.parseFromJson("instruction_set.json");
+        Assembler.compile(instructionSet, args[1]);
     }
 }
